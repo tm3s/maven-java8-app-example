ce380e812303d3e8ddc873348afbf75bfe0c2d87
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-25 14:53:19 +0100

    removed use of List.of

--- my-app-java8/src/main/java/com/mycompany/app/App.java
+++ my-app-java8/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,6 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,7 +9,12 @@ import java.util.List;
  */
 public class App {
 	public static void main(String[] args) {
-		List<String> words = List.of("Hello", "World!");
-		words.forEach(System.out::println);
+		List<String> words = new ArrayList<>();
+    	words.add("Hello");
+    	words.add("World!");
+    	// This will throw a NoSuchMethodError when
+    	// executed in Java 8:
+    	// List.of("Hello", "World!");
+        words.forEach(System.out::println);
 	}
 }
